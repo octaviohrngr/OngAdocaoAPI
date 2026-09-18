24b91d0cbae53ae2fd4c665a855e1c1836adf156
octaviohrngr <octavioheringer@hotmail.com>
2026-09-17 21:08:58 -0300

    correção do userRoutes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -7,8 +7,32 @@ import {
   atualizarUsuario,
   excluirUsuario,
 } from "../controllers/userController";
-;
 
 const router = Router();
+
+router.post(
+  "/",
+  criarUsuario
+);
+
+router.get(
+  "/",
+  listarUsuarios
+);
+
+router.get(
+  "/:id",
+  buscarUsuario
+);
+
+router.put(
+  "/:id",
+  atualizarUsuario
+);
+
+router.delete(
+  "/:id",
+  excluirUsuario
+);
 
 export default router;
